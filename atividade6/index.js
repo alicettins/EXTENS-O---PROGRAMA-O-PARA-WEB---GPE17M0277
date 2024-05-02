02c5ed6ed1762630e1e9495e954427469190cecf
ANA ALICE ALVES MARTINS <ana.amartins@a.ucb.br>
2024-05-02 10:34:15 -0300

    erros corrigidos

--- atividade6/index.js
+++ atividade6/index.js
@@ -1,18 +1,23 @@
- const express = require ('express');
- const mustacheExpress = require ('mustache-express');
- const app = express();
+const express           = require('express');
+const mustacheExpress   = require('mustache-express');
+const app               = express();
 
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
-app.get('/', function(req, res){
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', function (req, res) {
     res.render('index.html');
 });
 
-const PORT= 8080;
-
-app.listen(PORT, function(){
-    console.log('app exectando na porta'+ PORT);
+app.post('/dados', function (req, res) {
+    let dados = req.body;
+    res.render('dados.html', {dados});
 });
 
+const PORT = 8080;
+app.listen(PORT, function () {
+    console.log('app executando na porta ' + PORT);
+});
